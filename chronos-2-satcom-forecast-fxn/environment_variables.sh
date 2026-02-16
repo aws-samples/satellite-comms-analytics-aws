bc26830ba2b8f4e4d6703e09574402631aa139f5
Alan Campbell <122999933+alancaws@users.noreply.github.com>
2026-02-16 15:29:01 -0800

    Delete chronos-2-satcom-forecast-fxn directory

--- chronos-2-satcom-forecast-fxn/environment_variables.sh
+++
@@ -1,4 +0,0 @@
-export ENDPOINT_NAME="jumpstart-dft-pt-forecasting-chrono-20260216-185712"
-#export LAMBDA_FUNCTION_NAME="chronos-2-satcom-forecast-lambda"
-export LAMBDA_FUNCTION_NAME="satcom-chronos-forecast"
-export AWS_REGION="us-west-2"
